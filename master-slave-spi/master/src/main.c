ca87f1d237a93f50398d1ca3d10794f6ed1a3ebb
calebb <balboni.ca@northeastern.edu>
2026-01-26 19:58:19 -0500

    fixed everything for the spi interface

--- master-slave-spi/master/src/main.c
+++ master-slave-spi/master/src/main.c
@@ -3,50 +3,54 @@
 #include <zephyr/sys/printk.h>
 #include <string.h>
 
+#define WAKE_PIN 28
+
 #define SPI_OP (SPI_OP_MODE_MASTER | SPI_MODE_CPOL | SPI_MODE_CPHA | \
-                SPI_WORD_SET(8) | SPI_TRANSFER_MSB)
+                SPI_WORD_SET(8) | SPI_TRANSFER_MSB | SPI_LINES_SINGLE)
+
 
 static const struct spi_dt_spec master_dev =
-    SPI_DT_SPEC_GET(DT_NODELABEL(master_frdm), SPI_OP, 0);
-
+    SPI_DT_SPEC_GET(DT_NODELABEL(master_frdm), SPI_OP);
 
 int main(void)
 {
+    int pin_val = 0;
+
     if (!spi_is_ready_dt(&master_dev)) {
         printk("SPI device not ready\n");
         return 0;
     }
 
-    uint8_t data[2] = {0x00, 0x00};
+    uint8_t data[32] = "Hello slave!";
 
+    uint8_t rx[32];
 
     k_msleep(3000);
 
     while (1) {
-	struct spi_buf tx_buf = {
-		.buf = &data,
-		.len = 2,
-	};
+	    struct spi_buf tx_buf = {
+		    .buf = data,
+		    .len = sizeof(data),
+	    };
 
-	struct spi_buf_set tx_set = {
-		.buffers = &tx_buf,
-		.count = 1,
-	};
+	    struct spi_buf_set tx_set = {
+		    .buffers = &tx_buf,
+		    .count = 1,
+	    };
 
-	struct spi_buf rx_buf = {
-		.buf = NULL,
-		.len = 0,
-	};
+	    struct spi_buf rx_buf = {
+		    .buf = rx,
+		    .len = sizeof(rx),
+	    };
 
-	struct spi_buf_set rx_set = {
-		.buffers = &rx_buf,
-		.count = 0,
-	};
-        int rc = spi_write_dt(&master_dev, &tx_set);
-	data[0] += 0x10;
-	data[1] += 0x10;
-        printk("spi_write_dt rc=%d\n", rc);
-	k_msleep(100);
+	    struct spi_buf_set rx_set = {
+		    .buffers = &rx_buf,
+		    .count = 1,
+	    };
+      int rc = spi_transceive_dt(&master_dev, &tx_set, &rx_set);
+      printk("spi_write_dt rc=%d\n", rc);
+      printk("recieved: %s\n", (char*)rx);
+	    k_msleep(100);
     }
 
     return 0;
--- master-slave-spi/slave/src/main.c
+++ master-slave-spi/slave/src/main.c
@@ -1,12 +1,18 @@
 #include <zephyr/kernel.h>
+#include <zephyr/devicetree.h>
 #include <zephyr/drivers/spi.h>
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/sys/printk.h>
 
 #define SPI_OP (SPI_OP_MODE_SLAVE | SPI_MODE_CPOL | SPI_MODE_CPHA | \
-                SPI_WORD_SET(8) | SPI_TRANSFER_MSB)
+                SPI_WORD_SET(8) | SPI_TRANSFER_MSB | SPI_LINES_SINGLE)
+
+#define WAKE_PIN 28
 
 static const struct spi_dt_spec slave_dev =
-    SPI_DT_SPEC_GET(DT_NODELABEL(slave_frdm), SPI_OP, 0);
+    SPI_DT_SPEC_GET(DT_NODELABEL(slave_frdm), SPI_OP);
+
+//static const struct device* wakeup_gpio = DEVICE_DT_GET(DT_NODELABEL(gpio0));
 
 int main(void)
 {
@@ -14,12 +20,13 @@ int main(void)
         printk("SPI slave device not ready\n");
         return 0;
     }
-
-    uint8_t data[3]= {0x00, 0x00, 0x00};
+  
+    uint8_t data[32];
+    uint8_t tx[32] = "Hello master!";
 
     struct spi_buf rx_buf = {
         .buf = data,
-        .len = 2,
+        .len = sizeof(data),
     };
 
     struct spi_buf_set rx_set = {
@@ -28,20 +35,20 @@ int main(void)
     };
 
     struct spi_buf tx_buf = {
-	.buf = NULL,
-	.len = 0,
+	      .buf = tx,
+	      .len = sizeof(tx),
     };
 
     struct spi_buf_set tx_set = {
-	.buffers = &tx_buf,
-	.count = 0,
+	      .buffers = &tx_buf,
+	      .count = 1,
     };
 
     while (1) {
-        int rc = spi_write_dt(&slave_dev,&rx_set);
-
+        int rc = spi_transceive_dt(&slave_dev, &tx_set, &rx_set);
+        printk("\n");
         if (rc > 0) {
-		printk("hex 1: 0x%X, hex 2: 0x%X\n", data[0], data[1]);
+		      printk("data: %s\n", (char*)data);
         }
     }
 
