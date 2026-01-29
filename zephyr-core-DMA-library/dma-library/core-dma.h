083179ecda478454f2da601773ac858762d839c8
Caleb Balboni <117opchnhnx5eij81hhyr1vwkpz2bz@bots.bitbucket.org>
2026-01-29 18:04:35 -0500

    more work to driver

--- zephyr-core-DMA-library/dma-library/core-dma.h
+++ zephyr-core-DMA-library/dma-library/core-dma.h
@@ -2,11 +2,11 @@
 #include <zephyr/device.h>
 
 // wait for data to be recieved for a specified period of time, data passed in as a buffer
-typedef int (*sync_receive_t)(const struct device* dev, void* data, uint16_t receive_size, uint16_t wait_time);
+typedef int (*sync_receive_t)(const struct device* dev, void* data, size_t data_size, struct k_timeout_t wait_time);
 
 // don't wait for data to be received rather let the user pass in a callback function which will be
 // called when data is recieved.
-typedef int (*async_receive_t)(const struct device* dev, void (*func)(void*, void*), uint16_t receive_size, void* user_data);
+typedef int (*async_receive_t)(const struct device* dev, void (*callback_func)(void*, void*), size_t data_size, void* user_data);
 
 // send data
 typedef int (*send_t)(const struct device* dev, void* data, size_t data_size);
@@ -20,14 +20,14 @@ struct dma_engine {
 	init_t init;
 };
 
-static inline int dma_core_sync_receive(const struct device* dev, void* data, uint16_t wait_time) {
+static inline int dma_core_sync_receive(const struct device* dev, void* data, size_t data_size, struct k_timeout_t wait_time) {
 	const struct dma_engine* engine_api = (struct dma_engine*)dev->api;
-	return engine_api->sync_receive(dev, data, wait_time);
+	return engine_api->sync_receive(dev, data, receive_size, wait_time);
 }
 
-static inline int dma_core_async_receive(const struct device* dev, void (*func)(void*, void*), void* user_data) {
+static inline int dma_core_async_receive(const struct device* dev, void (*callback_func)(void*, void*), void* user_data) {
 	const struct dma_engine* engine_api = (struct dma_engine*)dev->api;
-	return engine_api->async_receive(dev, func, user_data);
+	return engine_api->async_receive(dev, callback_func, user_data);
 }
 
 static inline int dma_core_send(const struct device* dev, void* data, size_t data_size) {
